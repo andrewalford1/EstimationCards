42ed8f4ac3b98660c3fc8f5e5cfeeb1049f0c9f0
Andrew Alford <Andrew.Alford@tracerco.com>
2020-04-04 20:06:13 +0100

    Added content to the landing page

--- app/src/Pages/404.jsx
+++ app/src/Pages/404.jsx
@@ -4,14 +4,20 @@ import {Button} from '@material-ui/core'
 
 export default () => (
     <div className='Page'>
+
+        <header>
             <h1>Ah Crumpets! Looks like you got yourself lost</h1>
+        </header>
+
+        <body>
             <img src={Lost} 
-                alt={''} 
-                width={'75%'} 
-                height={'75%'}
+                 alt={''} 
+                 width={'75%'} 
+                 height={'75%'}
             />
             <Button color='primary' variant='outlined'>
                 <a href='/'>Home</a>
             </Button>
+        </body>
     </div>
 )
--- app/src/Pages/LandingPage.jsx
+++ app/src/Pages/LandingPage.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import {Button} from '@material-ui/core'
 
 export default () => (
     <div className='Page'>
         <h1>
-            Landing Page
+            Welcome
         </h1>
+        <p>
+        In Scrum Projects, Estimation is done by the entire team during 
+        Sprint Planning Meeting. The objective of the Estimation would 
+        be to consider the User Stories for the Sprint by Priority and
+        by the Ability of the team to deliver during the 
+        Time Box of the Sprint.
+        </p>
+        <p>
+        This application has been created enable software teams to 
+        effortlessly estimate backlog items in a virtual environment.
+        </p>
+        
+        <Button color='primary' variant='outlined'>
+            <a href='/'><a href='/RandomRoomId'>Start Estimating</a></a>
+        </Button>
     </div>
 )
