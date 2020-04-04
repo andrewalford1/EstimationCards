374c258398ac8a3e9ea3c85fa9b52868772ad382
Andrew Alford <Andrew.Alford@tracerco.com>
2020-04-04 12:26:02 +0100

    Added the room page with nav

--- app/src/App.js
+++ app/src/App.js
@@ -2,8 +2,6 @@ import React from 'react'
 import './App.css'
 import Routes from './Routes';
 
-console.log('hello world');
-
 function App() {
     return (
         <div className='App'>
--- app/src/Routes.jsx
+++ app/src/Routes.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
-import { Route, BrowserRouter as Router } from 'react-router-dom';
+import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
 import Room from './Pages/Room';
+import LandingPage from './Pages/LandingPage';
 
 export default () => (
 
     <Router>
-        <Route path="/">
-            <Room/>
-        </Route>
+        <Switch>
+            <route exact path='/'>
+                <LandingPage/>
+            </route>
+            
+            <Route path="/randomRoomID">
+                <Room/>
+            </Route>
+        </Switch>
     </Router>
 );
