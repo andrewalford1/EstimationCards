a19afed5c30ccc6a091a1e9ef3b3ee7fb7a9c9ef
Andrew Alford <Andrew.Alford@tracerco.com>
2020-04-04 11:09:08 +0100

    Created the host page

--- app/src/App.js
+++ app/src/App.js
@@ -1,12 +1,17 @@
 import React from 'react'
 import './App.css'
+import Host from './Pages/Host';
+
+console.log('hello world');
 
 function App() {
     return (
         <div className='App'>
             <h1>Estimation Station</h1>
+            
+            <Host/>
         </div>
-    )
+    );
 }
 
-export default App
+export default App;
