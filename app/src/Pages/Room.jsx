91f7f428d013575de07f079aa1b274811f0d5bc0
danielbutterfield <dan.butterfield@northumbria.ac.uk>
2020-04-04 13:49:08 +0100

    Add methods to return users for rooms

--- app/src/Pages/Room.jsx
+++ app/src/Pages/Room.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import Store, { getRealTimeUsersInRoom } from '../store/store'
 
-const Room = () => (
-    <div>
-        <h1>Room</h1>
-    </div>
-)
+const Room = () => {
+    const [users, setUsers] = useState({})
+    const [room, setRoom] = useState(1)
+
+    useEffect(() => {
+        getRealTimeUsersInRoom(room, (error, newUsers) => {
+            !error && setUsers(newUsers)
+        })
+    }, [room])
+
+    console.log('users', users)
+    return (
+        <div>
+            <h1>Room</h1>
+        </div>
+    )
+}
 
 export default Room
--- app/src/store/store.js
+++ app/src/store/store.js
@@ -3,7 +3,32 @@ import API from '../api'
 const rooms = API.firestore().collection('rooms')
 const users = API.firestore().collection('users')
 
-// export const getUsersInRoom
+export const getRealTimeUsersInRoom = (room, cb) => {
+    return users
+        .where('roomId', '==', room)
+        .orderBy('joinedAt')
+        .onSnapshot(async (querySnapshot) => {
+            const newUsers =
+                querySnapshot && querySnapshot.docs
+                    ? querySnapshot.docs.map((doc) => ({
+                          ...doc.data(),
+                          id: doc.id,
+                      }))
+                    : []
+
+            if (cb) {
+                cb(null, getUsersInRoom(newUsers))
+            }
+        })
+}
+
+export const getUsersInRoom = (users) => {
+    const sortedUsers = users.sort((firstUser, secondUser) => {
+        return firstUser.joinedAt.seconds > secondUser.joinedAt.seconds ? 1 : -1
+    })
+
+    return sortedUsers
+}
 // export const getNumbersChosenByUsers
 // export const getUserReadyStatus
 
