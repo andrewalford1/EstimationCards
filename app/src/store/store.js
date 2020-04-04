6f25bb27d5f96c6f401e074f459a158a2e207c52
danielbutterfield <dan.butterfield@northumbria.ac.uk>
2020-04-04 13:09:16 +0100

    Fix error in store path

--- app/src/store/store.js
+++ app/src/store/store.js
@@ -1,10 +1,10 @@
 import API from '../api'
 
 const rooms = API.firestore().collection('rooms')
-const numbers = API.firestore().collection('rooms')
+const users = API.firestore().collection('users')
 
 // export const getUsersInRoom
 // export const getNumbersChosenByUsers
 // export const getUserReadyStatus
 
-export default { rooms, numbers }
+export default { rooms, users }
