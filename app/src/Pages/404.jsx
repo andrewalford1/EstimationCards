33dd555c9dc4b4404b72103d25538b7b16dd7ed2
Andrew Alford <Andrew.Alford@tracerco.com>
2020-04-04 14:38:13 +0100

    work on styling

--- app/src/Pages/404.jsx
+++ app/src/Pages/404.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
+import Lost from '../assets/lost.svg'
 
 export default () => (
     <div>
-        <h1>404</h1>
+        <header>
+            <h1>404 - Go the fudge home</h1>
+        </header>
+        <img src={Lost} 
+             alt={''} 
+             width={'50%'} 
+             height={'50%'}
+        />
     </div>
 )
