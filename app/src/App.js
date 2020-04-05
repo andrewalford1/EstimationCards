d24c2017ed0423be6186030b9399ff8c887ddeb5
Andrew Alford <Andrew.Alford@tracerco.com>
2020-04-05 14:27:28 +0100

    remove whitespace

--- app/src/App.js
+++ app/src/App.js
@@ -15,7 +15,6 @@ const App = () => {
         <div className='App'>     
             <Routes/>
             <footer>
-
                 <div className='Credits'>
                     <Credit 
                     name='Andrew' 
