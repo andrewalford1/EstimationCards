6642817e3826b5af883b2cd69c214c5f258ccfed
Andrew Alford <Andrew.Alford@tracerco.com>
2020-04-05 13:58:36 +0100

    Added a footer to all pages

--- app/src/App.js
+++ app/src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import './App.css'
 import Store from './store/store'
 import Routes from './Routes'
+import Credit from './components/Credit'
 
 const App = () => {
     const rooms = Store.rooms
@@ -10,6 +11,12 @@ const App = () => {
     return (
         <div className='App'>     
             <Routes/>
+            <footer>
+                <Credit 
+                    name='Andrew Alford' 
+                    LinkedIn='https://www.linkedin.com/in/andrew-alford-479155152/'
+                />
+            </footer>
         </div>
     )
 }
