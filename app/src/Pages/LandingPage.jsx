421d5d48bbee875102eeab649061beb6302c836b
danielbutterfield <dan.butterfield@northumbria.ac.uk>
2020-04-10 06:06:25 +0100

    Update landing page structure and css

--- app/src/Pages/LandingPage.jsx
+++ app/src/Pages/LandingPage.jsx
@@ -8,27 +8,39 @@ export default () => (
 
         <div className='introText'>
             <p>
+                A flexible tool to provide quick estimation sessions, no
+                downloads and no tedious logins.
+            </p>
+            <p>
                 In Scrum Projects, Estimation is done by the entire team during
                 Sprint Planning Meeting. The objective of the Estimation would
                 be to consider the User Stories for the Sprint by Priority and
                 by the Ability of the team to deliver during the Time Box of the
                 Sprint.
             </p>
-            <p>
-                This application has been created enable software teams to
-                effortlessly estimate backlog items in a virtual environment
-                when remote working.
-            </p>
         </div>
 
         <div className='buttonChoices'>
-            <Button variant='contained' color='primary'>
-                <a href='/RandomRoomId'>Make a room</a>
-            </Button>
+            <div>
+                <p>
+                    You can create a room for your collegues and begin
+                    estimating stories quickly and easily!
+                </p>
 
-            <Button variant='contained' color='primary'>
-                <a href='/RandomRoomId'>Join a room</a>
-            </Button>
+                <Button variant='contained' color='primary'>
+                    <a href='/RandomRoomId'>Create a room</a>
+                </Button>
+            </div>
+
+            <div>
+                <p>
+                    Do you already know a room has been set up? Well, you can
+                    join it by entering the URL here too!
+                </p>
+                <Button variant='contained' color='primary'>
+                    <a href='/RandomRoomId'>Join a room</a>
+                </Button>
+            </div>
         </div>
 
         <img src={Remote} alt={'Woman sitting at her desk'} />
