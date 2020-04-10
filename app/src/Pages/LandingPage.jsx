3f6af72b058c6ebc15afab08d7b04ecbbae2afd0
danielbutterfield <dan.butterfield@northumbria.ac.uk>
2020-04-10 05:45:34 +0100

    Update buttons and css

--- app/src/Pages/LandingPage.jsx
+++ app/src/Pages/LandingPage.jsx
@@ -1,32 +1,30 @@
 import React from 'react'
 import Remote from '../assets/remote.svg'
-import {Button} from '@material-ui/core'
+import { Button } from '@material-ui/core'
 
 export default () => (
     <div className='Page'>
-        <h1>
-            Welcome
-        </h1>
+        <h1>Welcome</h1>
 
-        <div id='intro_text'>
+        <div id='IntroText'>
             <p>
-            In Scrum Projects, Estimation is done by the entire team during 
-            Sprint Planning Meeting. The objective of the Estimation would 
-            be to consider the User Stories for the Sprint by Priority and
-            by the Ability of the team to deliver during the 
-            Time Box of the Sprint.
+                In Scrum Projects, Estimation is done by the entire team during
+                Sprint Planning Meeting. The objective of the Estimation would
+                be to consider the User Stories for the Sprint by Priority and
+                by the Ability of the team to deliver during the Time Box of the
+                Sprint.
             </p>
             <p>
-            This application has been created enable software teams to 
-            effortlessly estimate backlog items in a virtual environment 
-            when remote working.
+                This application has been created enable software teams to
+                effortlessly estimate backlog items in a virtual environment
+                when remote working.
             </p>
         </div>
 
-        <img src={Remote} alt={''}/>
-        
-        <Button color='primary' variant='outlined'>
-            <a href='/'><a href='/RandomRoomId'>Start Estimating</a></a>
+        <Button variant='contained' color='primary'>
+            <a href='/RandomRoomId'>Start Estimating</a>
         </Button>
+
+        <img src={Remote} alt={''} />
     </div>
 )
