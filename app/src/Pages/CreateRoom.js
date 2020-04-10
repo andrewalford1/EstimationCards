32cc60f39c5d69f2113f2d7d289df089de2166ec
danielbutterfield <dan.butterfield@northumbria.ac.uk>
2020-04-10 12:35:57 +0100

    Add room creation and Edit landing page

--- app/src/Pages/CreateRoom.js
+++ app/src/Pages/CreateRoom.js
@@ -1,7 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { createRoom } from '../store/store'
+import { nanoid } from 'nanoid'
+import { Button, TextField } from '@material-ui/core'
 
 const CreateRoom = () => {
-    return <div>Create Room</div>
+    const [roomName, setRoomName] = useState('')
+
+    const handleCreateRoom = async () => {
+        const roomId = nanoid()
+        await createRoom({ roomId, roomName })
+    }
+
+    const handleNameChange = (event) => {
+        const name = event.target.value
+        setRoomName(name)
+    }
+
+    return (
+        <div className='fakeCenterSection'>
+            <div className='whitePopoutBox'>
+                <form id='createRoom'>
+                    <TextField
+                        id='standard-basic'
+                        label='Room name'
+                        onChange={handleNameChange}
+                        value={roomName}
+                    />
+                    <Button
+                        variant='contained'
+                        color='default'
+                        onClick={handleCreateRoom}
+                    >
+                        Create Room
+                    </Button>
+                </form>
+            </div>
+        </div>
+    )
 }
 
 export default CreateRoom
